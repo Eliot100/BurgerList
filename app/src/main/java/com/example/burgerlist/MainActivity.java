929796caea0bdaeb4aa6994cfe15cfc38b249c56
AlexVaisman <alex.vaisman1@gmail.com>
2020-11-20 13:17:39 +0200

    testing

--- app/src/main/java/com/example/burgerlist/MainActivity.java
+++ app/src/main/java/com/example/burgerlist/MainActivity.java
@@ -2,14 +2,42 @@ package com.example.burgerlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
+Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //testtttt1!!!
+
+        login_button = (Button)findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, Login_activity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
+
+
 }
