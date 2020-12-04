010484900a836bb72596c22e026976861a02f278
Eli Ruvinov <eli4056@gmail.com>
2020-12-04 12:36:31 +0200

    commit - List Activity

--- app/src/main/java/com/example/burgerlist/MainActivity.java
+++ app/src/main/java/com/example/burgerlist/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button login_button;
+    Button userListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +27,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        userListButton = (Button)findViewById(R.id.userListButton);
+
+        userListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start_ListPage();
+            }
+        });
 
     }
 
     private void start_Login() {
         Intent intent = new Intent(this, Login.class);
         Toast.makeText(getApplicationContext(),"ke1", Toast.LENGTH_SHORT).show();
+        startActivity(intent);
+    }
+
+    private void start_ListPage() {
+        Intent intent = new Intent(this, ListPage.class);
+        Toast.makeText(getApplicationContext(),"ke2", Toast.LENGTH_SHORT).show();
         startActivity(intent);
     }
 
