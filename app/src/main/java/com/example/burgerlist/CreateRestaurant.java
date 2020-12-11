a88ba892215801d00d75decbc800a69a454a40a1
AlexVaisman <alex.vaisman1@gmail.com>
2020-12-11 19:51:56 +0200

    fixed bugs in search by city, changed restarunt creation, can no longer type city. need to search for it.

--- app/src/main/java/com/example/burgerlist/CreateRestaurant.java
+++ app/src/main/java/com/example/burgerlist/CreateRestaurant.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -16,6 +17,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Patterns;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -81,6 +83,15 @@ public class CreateRestaurant extends AppCompatActivity {
         });
 
 
+        Restcity.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                start_search();
+                return false;
+            }
+        });
+
         createRestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -93,6 +104,27 @@ public class CreateRestaurant extends AppCompatActivity {
             }
         });
     }
+
+    private void start_search(){
+        Intent intent = new Intent(this, Search.class);
+        startActivityForResult(intent,1);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK){
+                Restcity.setText(data.getStringExtra("Search_Res"));
+            }
+            if (resultCode == RESULT_CANCELED) {
+                //might be usefull later
+            }
+        }
+
+
+    }
+
 
 
 
--- app/src/main/java/com/example/burgerlist/Search.java
+++ app/src/main/java/com/example/burgerlist/Search.java
@@ -2,10 +2,15 @@ package com.example.burgerlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 
 import java.util.ArrayList;
@@ -24,34 +29,34 @@ public class Search extends AppCompatActivity {
         listView =(ListView)findViewById(R.id.list_view);
         searchView = (SearchView)findViewById(R.id.search_view);
         citys = new ArrayList<String>();
+        ArrayList<String> filterd_list = new ArrayList<String>();
 
         //reading csv file of all israel citys
          // will be done later
 
          //place holder for citys
-        citys.add("ירושלים");
-        citys.add("תל אביב");
-        citys.add("הוד השרון");
-        citys.add("הרצליה");
-        citys.add("רעננה");
-        citys.add("כפר סבא");
-        citys.add("נתניה");
-        citys.add("פתח תקווה");
-        citys.add("חדרה");
-        citys.add("חיפה");
-        citys.add("טבריה");
-        citys.add("אילת");
-        citys.add("שדרות");
-        citys.add("כפר הנשיא");
+        citys.add("jerusalem");
+        citys.add("tel aviv");
+        citys.add("hod hasharon");
+        citys.add("herzliya");
+        citys.add("Raanana");
+        citys.add("kfar saba");
+        citys.add("natania");
+        citys.add("petah tikva");
+        citys.add("hadera");
+        citys.add("haifa");
+        citys.add("Tiberias");
+        citys.add("eilat");
+        citys.add("Sderot");
+        citys.add("kfar hanassi");
 
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,citys);
+
+
         listView.setAdapter(adapter);
-
-
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-
                 return false;
             }
 
@@ -61,6 +66,38 @@ public class Search extends AppCompatActivity {
                 return false;
             }
         });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                filterd_list.clear();
+                for (int i = 0 ; i < adapter.getCount() ; i++){
+                    filterd_list.add(adapter.getItem(i).toString());
+                    //Toast.makeText(getApplicationContext(),filterd_list.get(i), Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(getApplicationContext(),filterd_list.get(position), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.putExtra("Search_Res", filterd_list.get(position));
+                setResult(Activity.RESULT_OK,intent);
+                finish();
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
