49e7b7678793d2a9a24f2b30b540ab0aa4e25857
AlexVaisman <alex.vaisman1@gmail.com>
2020-12-12 20:33:51 +0200

    working on rest page

--- app/src/main/java/com/example/burgerlist/RestPage.java
+++ app/src/main/java/com/example/burgerlist/RestPage.java
@@ -3,6 +3,12 @@ package com.example.burgerlist;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,10 +23,19 @@ public class RestPage extends AppCompatActivity {
     private String rest_owner_id;
     private String rest_name;
     private String rest_phone;
-    private ArrayList<String> rest_comments;
     private String rest_rating;
 
+    private ImageButton phone_btn;
+    private ImageButton map_btn;
+    private Button addcomment_btn;
+    private EditText comment_text;
+    private TextView ratingscore_text;
+    private TextView workinghours_text;
+    private TextView restname_title;
+    private ScrollView menu_scrollview;
+    private ScrollView comment_scrollview;
 
+    private ArrayList<String> rest_comments;
 
 
     private FirebaseDatabase database;
@@ -31,11 +46,25 @@ public class RestPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest_page);
-
+        //initializing variable
         database = FirebaseDatabase.getInstance();
         ref = database.getReference("Restaurants").child(this.getIntent().getStringExtra("Owner_id"));
+        phone_btn = (ImageButton)findViewById(R.id.phone_button);
+        map_btn = (ImageButton)findViewById(R.id.google_map);
+        addcomment_btn = (Button)findViewById(R.id.addComent_button);
+        comment_text = (EditText)findViewById(R.id.addcomment_text);
+        ratingscore_text = (TextView)findViewById(R.id.currentReating_text);
+       // workinghours_text = (TextView)findViewById(R.id.workinghours_text);
+        menu_scrollview = (ScrollView)findViewById(R.id.menu_scrollview);
+        comment_scrollview = (ScrollView)findViewById(R.id.comment_scrollview);
+        restname_title = (TextView)findViewById(R.id.restName_text);
 
+
+        // getting data from data base.
         get_rest_data();
+
+        //checking if current user is resturant owner
+
 
 
 
@@ -51,12 +80,20 @@ public class RestPage extends AppCompatActivity {
                 rest_owner_id =  snapshot.child("ownerId").getValue().toString();
                 rest_phone = snapshot.child("phone").getValue().toString();
                 rest_rating = snapshot.child("currentRating").getValue().toString();
+                update_page();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
+
         });
+    }
+
+    private void update_page(){
+        restname_title.setText(rest_name);
+        ratingscore_text.setText(rest_rating);
+
     }
 }
