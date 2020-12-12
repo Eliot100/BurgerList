b7d247904c06fcbf5ea919ceb5137530633b4b10
AlexVaisman <alex.vaisman1@gmail.com>
2020-12-12 21:10:42 +0200

    Can now post comments on resturant page

--- app/src/main/java/com/example/burgerlist/RestPage.java
+++ app/src/main/java/com/example/burgerlist/RestPage.java
@@ -9,7 +9,10 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,12 +21,15 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class RestPage extends AppCompatActivity {
     private String rest_owner_id;
     private String rest_name;
     private String rest_phone;
     private String rest_rating;
+    private boolean Current_User_Is_owner;
+    private boolean Current_User_Loggedin;
 
     private ImageButton phone_btn;
     private ImageButton map_btn;
@@ -54,16 +60,17 @@ public class RestPage extends AppCompatActivity {
         addcomment_btn = (Button)findViewById(R.id.addComent_button);
         comment_text = (EditText)findViewById(R.id.addcomment_text);
         ratingscore_text = (TextView)findViewById(R.id.currentReating_text);
-       // workinghours_text = (TextView)findViewById(R.id.workinghours_text);
+        workinghours_text = (TextView)findViewById(R.id.workinghours_text);
         menu_scrollview = (ScrollView)findViewById(R.id.menu_scrollview);
         comment_scrollview = (ScrollView)findViewById(R.id.comment_scrollview);
         restname_title = (TextView)findViewById(R.id.restName_text);
 
 
+
         // getting data from data base.
         get_rest_data();
 
-        //checking if current user is resturant owner
+
 
 
 
@@ -80,6 +87,9 @@ public class RestPage extends AppCompatActivity {
                 rest_owner_id =  snapshot.child("ownerId").getValue().toString();
                 rest_phone = snapshot.child("phone").getValue().toString();
                 rest_rating = snapshot.child("currentRating").getValue().toString();
+
+                check_user_is_owner();
+                check_user_loggeding();
                 update_page();
             }
 
@@ -95,5 +105,36 @@ public class RestPage extends AppCompatActivity {
         restname_title.setText(rest_name);
         ratingscore_text.setText(rest_rating);
 
+        addcomment_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Current_User_Loggedin){
+                    Date d = java.util.Calendar.getInstance().getTime();
+
+                    ref.child("Comments").child(MainActivity.user_id).child(d.toString()).setValue(comment_text.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            Toast.makeText(getApplicationContext(), "Post successful", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "You need to be logged in to leave a comment", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+    private void check_user_is_owner(){
+        if(rest_owner_id.equals(MainActivity.get_user_id()) == true){
+            Current_User_Is_owner = true;
+        }
+    }
+    private void check_user_loggeding(){
+        if(MainActivity.get_user_id().length()>6){
+            Current_User_Loggedin = true;
+        }
     }
 }
